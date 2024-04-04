1a866b1b5eac4c760ec006b3eb694aa084e90152
Manisha <mani0076@algonquinlive.com>
2024-04-04 19:37:52 -0400

    final steps

---
+++ app/src/main/java/binding/RecyclerView.java
@@ -0,0 +1,10 @@
+package binding;
+
+public class RecyclerView {
+    
+    
+    
+    
+    
+    
+}
---
+++ app/src/test/java/data/ChatMessage2_ImplTest.java
@@ -0,0 +1,7 @@
+package data;
+
+import junit.framework.TestCase;
+
+public class ChatMessage2_ImplTest extends TestCase {
+
+}
